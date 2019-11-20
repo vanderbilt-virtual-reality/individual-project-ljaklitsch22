17cffe53d87587b522654bbb1c64239fad312a86
lukas <lukas.jaklitsch@vanderbilt.edu>
2019-11-19 22:52:41 -0600

    Button

---
+++ Assets/SCRIPTS/BOX_DESTRUCTABLE.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOX_DESTRUCTABLE : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+
+    //change to on collision
+    private void OnMouseEnter()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+    
+}
---
+++ Assets/SCRIPTS/button_push.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_push : MonoBehaviour
+{
+    public GameObject bomb;
+
+	[SerializeField]
+	private Transform spawnPoint;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+    
+    void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "BUTTON")
+		{
+			Instantiate(bomb, new Vector3(0,0,0), Quaternion.identity);
+		}
+	}
+}
